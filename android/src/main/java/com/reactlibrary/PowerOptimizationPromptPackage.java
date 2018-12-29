9efc3bc17667a97bf25e5fb0e12bf2dda6c6963a
gentealliage <gentealliage@hotmail.com>
2018-12-29 14:28:14 +0100

    added android code

--- android/src/main/java/com/reactlibrary/PowerOptimizationPromptPackage.java
+++ android/src/main/java/com/reactlibrary/PowerOptimizationPromptPackage.java
@@ -10,10 +10,11 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
-public class RNReactNativePowerOptimizationPromptPackage implements ReactPackage {
+
+public class PowerOptimizationPromptPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNReactNativePowerOptimizationPromptModule(reactContext));
+      return Arrays.<NativeModule>asList(new PowerOptimizationPromptModule(reactContext));
     }
 
     // Deprecated from RN 0.47
